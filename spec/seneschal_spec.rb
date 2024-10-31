4a0deb42d4bf0c0f6815c1d136eb8d6086a48aa7
Israel De La Hoz <israeldelahoz@gmail.com>
2024-10-31 09:19:44 -0500

    initial commit

---
+++ spec/seneschal_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Seneschal do
+  it "has a version number" do
+    expect(Seneschal::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
